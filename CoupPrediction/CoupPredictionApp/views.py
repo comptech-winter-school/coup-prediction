26c6a7f5cc648d29e5fc8df86d156e2091820915
DK <dk.promsoft@mail.ru>
2021-02-04 22:44:45 +0700

    СДелал макет на бутстрапе

--- CoupPrediction/CoupPredictionApp/views.py
+++ CoupPrediction/CoupPredictionApp/views.py
@@ -9,14 +9,6 @@ import plotly.express as px
 
 # Create your views here.
 def main_page(request):
-    return render(request, 'CoupPredictionApp/main.html')
-
-
-def datasets_page(request):
-    return render(request, 'CoupPredictionApp/Review.html')
-
-
-def index(request):
     response = open(os.path.dirname(os.path.realpath(__file__)) + '/geo.json', "r")
     counties = json.load(response)
 
@@ -30,3 +22,7 @@ def index(request):
                         )
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     return render(request, 'CoupPredictionApp/main.html', context={'plot_div': plot(fig, output_type='div')})
+
+
+def datasets_page(request):
+    return render(request, 'CoupPredictionApp/datasets.html')
