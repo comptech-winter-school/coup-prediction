fbce38f1f5d92df63a6f2d40ba0f8c74ac324b39
«Eugene <e.pavlov98@mail.ru>
2021-02-06 01:31:27 +0700

    fixed endpoints

--- CoupPrediction/CoupPredictionApp/urls.py
+++ CoupPrediction/CoupPredictionApp/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main'),
-    path('datasets', views.datasets_page, name='datasets'),
-    path('index', views.index, name='index')
+    path('datasets', views.datasets_page, name='datasets')
 ]
--- CoupPrediction/CoupPredictionApp/views.py
+++ CoupPrediction/CoupPredictionApp/views.py
@@ -8,15 +8,11 @@ import plotly.express as px
 
 
 # Create your views here.
-def main_page(request):
-    return render(request, 'CoupPredictionApp/main.html')
-
-
 def datasets_page(request):
     return render(request, 'CoupPredictionApp/Review.html')
 
 
-def index(request):
+def main_page(request):
     response = open(os.path.dirname(os.path.realpath(__file__)) + '/geo.json', "r")
     counties = json.load(response)
 
